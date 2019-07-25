284a52bcabb75b73cc40d9c633d3a4687f31051a
noahfranco <noahfra@gmail.com>
2019-07-25 05:18:33 -0400

    debuging issue with display user function

--- team/src/components/Form.js
+++ team/src/components/Form.js
@@ -1,29 +1,34 @@
 import React, { useState } from "react"; 
-
+// import styling, { container } from "./styling"
  
 function FormList() {
 
 const [teamMembers, setTeamMembers] = useState({Name: "", Email: ""});
 
+
 function handleChange(event) {
 
-// This is seting up the user 
+// This is seting up the user and email 
     const changingHandler = { ...teamMembers, [event.target.name]: event.target.value }
     console.log(
-        "user change",
+        "handleChage",
         event.target.name,
-        event.target.value
+        event.target.value,
+        changingHandler
     );
     setTeamMembers(changingHandler); 
 // This is seting up the user 
 }
 
-
-
-
+// This is seting up the button to work 
+function submit(now) {
+    now.preventDefault();
+   console.log("username", teamMembers)
+}
+// This is seting up the button to work 
 
     return(
-        <form> 
+        <form onSubmit={submit}> 
             <fieldset> 
             <legend> Sign Up here </legend> 
             <div> 
@@ -31,7 +36,7 @@ function handleChange(event) {
                     <p> Username </p>
                 </div> 
                 <div>
-                    <input type="text" placeholder="Username" onChange="handleChange"/> 
+                    <input type="text" name="username" placeholder="Username" value={teamMembers.username}  onChange={handleChange} /> 
                  </div> 
             </div>
             <div> 
@@ -39,12 +44,12 @@ function handleChange(event) {
                     <p> Email </p> 
                  </div>
                 <div> 
-                    <input type="email" placeholder="Email" /> 
+                    <input type="email" name="email" placeholder="Email" value={teamMembers.email} onChange={handleChange} /> 
                 </div>
             </div>
             <div>
                <div>
-                    <button> Submit </button>  
+                    <button type="submit"> Submit </button>  
                 </div> 
             </div> 
             </fieldset>
