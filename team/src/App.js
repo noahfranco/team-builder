1bf3203c2b8e1572695c85b98f2baa7278b379ac
noahfranco <noahfra@gmail.com>
2019-07-25 23:25:31 -0400

    starting over

--- team/src/App.js
+++ team/src/App.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import Form from "./components/Form"
+
+
 
 
 function App() {
-  
   return(
-<div>
-    <h1> Become Part Of the Team </h1>
-    <Form /> 
-  </div>
+<div className="App">
+  <div> 
+    <h2> Become Part Of The Team </h2> 
+  </div> 
+</div>
 
   );
-}
+};
 
 export default App;
--- team/src/components/styling.js
+++
@@ -1,5 +0,0 @@
-// import React from "react"; 
-
-// export const container = `
-//     background: green; 
-// `
