6a350dce9406411414cfa6c29bde68daf69ee156
noahfranco <noahfra@gmail.com>
2019-07-25 23:52:05 -0400

    added to App.js file and created component with file called TeamMemberList.js

--- team/src/App.js
+++ team/src/App.js
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
 import './App.css';
-
+import TeamMemberList from "./components/TeamMemberList.js"
 
 
 
 function App() {
+
+  const [members, setMembers] = useState([{name: "Noah", email: "Noahfra@gmail.com", role: "Developer" }])
+
   return(
 <div className="App">
   <div> 
     <h2> Become Part Of The Team </h2> 
+    <TeamMemberList members={ members } /> 
   </div> 
 </div>
 
---
+++ team/src/components/TeamMemberList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+ function TeamMemberList(props) {
+    return (
+        <div>
+            <p> Hello There </p> 
+        </div>
+    )
+}
+
+export default TeamMemberList; 
